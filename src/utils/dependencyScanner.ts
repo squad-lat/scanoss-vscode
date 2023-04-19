00bc5a8c37aa5979f6872592538a3e6e158d1ade
agustinfranchetti <agustinfranchetti@gmail.com>
2023-04-19 12:14:47 -0300

    fix: rm unnecesary scanner files

--- src/utils/dependencyScanner.ts
+++ src/utils/dependencyScanner.ts
@@ -1,21 +1,15 @@
 import fs from 'fs';
-import { DependencyScanner, DependencyScannerCfg } from 'scanoss';
+import { DependencyScanner } from 'scanoss';
 import { getRootProjectFolder } from './sdk';
 
 export const scanDependencies = async (allFiles: string[]) => {
   try {
-    // const allFiles: string[] = [];
     const rootFolder = await getRootProjectFolder();
     const rootPath = rootFolder + '/';
 
-    const cfg = new DependencyScannerCfg();
-    //TODO: get PAC and PROXY from user settings @agus. Definir userSettings as well
-    // const { PAC, PROXY } = await userSettingService.get();
-    // cfg.PAC = PAC;
-    // cfg.PROXY = PROXY;
-    await cfg.validate();
+    const dependencyScanner = new DependencyScanner();
 
-    const dependencies = await new DependencyScanner().scan(allFiles);
+    const dependencies = await dependencyScanner.scan(allFiles);
     dependencies.filesList.forEach((f) => {
       f.file = f.file.replace(rootPath, '');
     });
